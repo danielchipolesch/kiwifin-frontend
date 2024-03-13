bb50ab780d0e778b5255bf4d76e4e9c69f739663
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-03-13 19:51:28 -0300

    Retirado o ícone/botão de calendário da barra de topo

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -84,17 +84,17 @@ const isOutsideClicked = (event) => {
         </button>
 
         <div class="layout-topbar-menu" :class="topbarMenuClasses">
-            <button @click="onTopBarMenuButton()" class="p-link layout-topbar-button">
+            <!-- <button @click="onTopBarMenuButton()" class="p-link layout-topbar-button">
                 <i class="pi pi-calendar"></i>
                 <span>Calendar</span>
+            </button> -->
+            <button @click="onSettingsClick()" class="p-link layout-topbar-button">
+                <i class="pi pi-cog"></i>
+                <span>Settings</span>
             </button>
             <button @click="meuCadastroClick()" class="p-link layout-topbar-button">
                 <i class="pi pi-user"></i>
                 <span>Profile</span>
-            </button>
-            <button @click="onSettingsClick()" class="p-link layout-topbar-button">
-                <i class="pi pi-cog"></i>
-                <span>Settings</span>
             </button>
         </div>
     </div>
