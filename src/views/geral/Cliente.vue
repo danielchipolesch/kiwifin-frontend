9c3f996fd526616946d70743aed77e67034d1b79
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-03-13 22:29:18 -0300

    Inclusão dos componentes padronizados nas páginas pendentes de finalizão.

--- src/views/geral/Cliente.vue
+++ src/views/geral/Cliente.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1>Novo Cliente</h1>
+    <Toolbar class="mb-4 bg-green-100">
+		<template v-slot:start>
+			<div class="my-2">
+				<span class="text-black-alpha-90 text-3xl">Clientes</span>
+			</div>
+		</template>
+	</Toolbar>
 </template>
--- src/views/geral/Motivo.vue
+++ src/views/geral/Motivo.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1>Novo Motivo</h1>
+    <Toolbar class="mb-4 bg-green-100">
+		<template v-slot:start>
+			<div class="my-2">
+				<span class="text-black-alpha-90 text-3xl">Motivos</span>
+			</div>
+		</template>
+	</Toolbar>
 </template>
