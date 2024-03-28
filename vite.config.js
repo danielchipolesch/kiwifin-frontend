9d6e30fe35ca35f8a335c68870599f0a5583045b
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-03-28 01:30:53 -0300

    Melhoria de código

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-    return {
-        plugins: [vue()],
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url))
-            }
-        }
-    };
+	return {
+		plugins: [vue()],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url))
+			}
+		}
+	};
 });
