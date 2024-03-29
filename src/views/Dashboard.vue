ef56ae339d9e1f1c633d542e28371c36d7468f19
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-03-29 15:55:17 -0300

    Excluídos os cards sem função

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,177 +7,177 @@ const { isDarkTheme } = useLayout();
 
 const products = ref(null);
 const lineData = reactive({
-    labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho'],
-    datasets: [
-        {
-            label: 'First Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            backgroundColor: '#2f4860',
-            borderColor: '#2f4860',
-            tension: 0.4
-        },
-        {
-            label: 'Second Dataset',
-            data: [28, 48, 40, 19, 86, 27, 90],
-            fill: false,
-            backgroundColor: '#00bb7e',
-            borderColor: '#00bb7e',
-            tension: 0.4
-        }
-    ]
+	labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho'],
+	datasets: [
+		{
+			label: 'First Dataset',
+			data: [65, 59, 80, 81, 56, 55, 40],
+			fill: false,
+			backgroundColor: '#2f4860',
+			borderColor: '#2f4860',
+			tension: 0.4
+		},
+		{
+			label: 'Second Dataset',
+			data: [28, 48, 40, 19, 86, 27, 90],
+			fill: false,
+			backgroundColor: '#00bb7e',
+			borderColor: '#00bb7e',
+			tension: 0.4
+		}
+	]
 });
 const items = ref([
-    { label: 'Add New', icon: 'pi pi-fw pi-plus' },
-    { label: 'Remove', icon: 'pi pi-fw pi-minus' }
+	{ label: 'Add New', icon: 'pi pi-fw pi-plus' },
+	{ label: 'Remove', icon: 'pi pi-fw pi-minus' }
 ]);
 const lineOptions = ref(null);
 const productService = new ProductService();
 
 onMounted(() => {
-    productService.getProductsSmall().then((data) => (products.value = data));
+	productService.getProductsSmall().then((data) => (products.value = data));
 });
 
 const formatCurrency = (value) => {
-    return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+	return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 };
 const applyLightTheme = () => {
-    lineOptions.value = {
-        plugins: {
-            legend: {
-                labels: {
-                    color: '#495057'
-                }
-            }
-        },
-        scales: {
-            x: {
-                ticks: {
-                    color: '#495057'
-                },
-                grid: {
-                    color: '#ebedef'
-                }
-            },
-            y: {
-                ticks: {
-                    color: '#495057'
-                },
-                grid: {
-                    color: '#ebedef'
-                }
-            }
-        }
-    };
+	lineOptions.value = {
+		plugins: {
+			legend: {
+				labels: {
+					color: '#495057'
+				}
+			}
+		},
+		scales: {
+			x: {
+				ticks: {
+					color: '#495057'
+				},
+				grid: {
+					color: '#ebedef'
+				}
+			},
+			y: {
+				ticks: {
+					color: '#495057'
+				},
+				grid: {
+					color: '#ebedef'
+				}
+			}
+		}
+	};
 };
 
 const applyDarkTheme = () => {
-    lineOptions.value = {
-        plugins: {
-            legend: {
-                labels: {
-                    color: '#ebedef'
-                }
-            }
-        },
-        scales: {
-            x: {
-                ticks: {
-                    color: '#ebedef'
-                },
-                grid: {
-                    color: 'rgba(160, 167, 181, .3)'
-                }
-            },
-            y: {
-                ticks: {
-                    color: '#ebedef'
-                },
-                grid: {
-                    color: 'rgba(160, 167, 181, .3)'
-                }
-            }
-        }
-    };
+	lineOptions.value = {
+		plugins: {
+			legend: {
+				labels: {
+					color: '#ebedef'
+				}
+			}
+		},
+		scales: {
+			x: {
+				ticks: {
+					color: '#ebedef'
+				},
+				grid: {
+					color: 'rgba(160, 167, 181, .3)'
+				}
+			},
+			y: {
+				ticks: {
+					color: '#ebedef'
+				},
+				grid: {
+					color: 'rgba(160, 167, 181, .3)'
+				}
+			}
+		}
+	};
 };
 
 watch(
-    isDarkTheme,
-    (val) => {
-        if (val) {
-            applyDarkTheme();
-        } else {
-            applyLightTheme();
-        }
-    },
-    { immediate: true }
+	isDarkTheme,
+	(val) => {
+		if (val) {
+			applyDarkTheme();
+		} else {
+			applyLightTheme();
+		}
+	},
+	{ immediate: true }
 );
 </script>
 
 <template>
-    <div class="grid">
-        <div class="col-12 lg:col-6 xl:col-3">
-            <div class="card mb-0">
-                <div class="flex justify-content-between mb-3">
-                    <div>
-                        <span class="block text-500 font-medium mb-3">Abertos</span>
-                        <div class="text-900 font-medium text-xl">152</div>
-                    </div>
-                    <div class="flex align-items-center justify-content-center bg-blue-100 border-round" style="width: 2.5rem; height: 2.5rem">
-                        <i class="pi pi-inbox text-blue-500 text-xl"></i>
-                    </div>
-                </div>
-                <span class="text-green-500 font-medium">24 </span>
-                <span class="text-500">novos</span>
-            </div>
-        </div>
-        <div class="col-12 lg:col-6 xl:col-3">
-            <div class="card mb-0">
-                <div class="flex justify-content-between mb-3">
-                    <div>
-                        <span class="block text-500 font-medium mb-3">Atendidos</span>
-                        <div class="text-900 font-medium text-xl">2.100</div>
-                    </div>
-                    <div class="flex align-items-center justify-content-center bg-orange-100 border-round" style="width: 2.5rem; height: 2.5rem">
-                        <i class="pi pi-flag text-orange-500 text-xl"></i>
-                    </div>
-                </div>
-                <span class="text-green-500 font-medium">52 </span>
-                <span class="text-500">novos</span>
-            </div>
-        </div>
-        <div class="col-12 lg:col-6 xl:col-3">
-            <div class="card mb-0">
-                <div class="flex justify-content-between mb-3">
-                    <div>
-                        <span class="block text-500 font-medium mb-3">Reabertos</span>
-                        <div class="text-900 font-medium text-xl">28</div>
-                    </div>
-                    <div class="flex align-items-center justify-content-center bg-cyan-100 border-round" style="width: 2.5rem; height: 2.5rem">
-                        <i class="pi pi-comment text-cyan-500 text-xl"></i>
-                    </div>
-                </div>
-                <span class="text-green-500 font-medium">0 </span>
-                <span class="text-500">novos</span>
-            </div>
-        </div>
-        <div class="col-12 lg:col-6 xl:col-3">
-            <div class="card mb-0">
-                <div class="flex justify-content-between mb-3">
-                    <div>
-                        <span class="block text-500 font-medium mb-3">Concluídos</span>
-                        <div class="text-900 font-medium text-xl">152</div>
-                    </div>
-                    <div class="flex align-items-center justify-content-center bg-purple-100 border-round" style="width: 2.5rem; height: 2.5rem">
-                        <i class="pi pi-thumbs-up text-purple-500 text-xl"></i>
-                    </div>
-                </div>
-                <span class="text-green-500 font-medium">85 </span>
-                <span class="text-500">novos</span>
-            </div>
-        </div>
+	<div class="grid">
+		<div class="col-12 lg:col-6 xl:col-3">
+			<div class="card mb-0">
+				<div class="flex justify-content-between mb-3">
+					<div>
+						<span class="block text-500 font-medium mb-3">Abertos</span>
+						<div class="text-900 font-medium text-xl">152</div>
+					</div>
+					<div class="flex align-items-center justify-content-center bg-blue-100 border-round" style="width: 2.5rem; height: 2.5rem">
+						<i class="pi pi-inbox text-blue-500 text-xl"></i>
+					</div>
+				</div>
+				<span class="text-green-500 font-medium">24 </span>
+				<span class="text-500">novos</span>
+			</div>
+		</div>
+		<div class="col-12 lg:col-6 xl:col-3">
+			<div class="card mb-0">
+				<div class="flex justify-content-between mb-3">
+					<div>
+						<span class="block text-500 font-medium mb-3">Atendidos</span>
+						<div class="text-900 font-medium text-xl">2.100</div>
+					</div>
+					<div class="flex align-items-center justify-content-center bg-orange-100 border-round" style="width: 2.5rem; height: 2.5rem">
+						<i class="pi pi-flag text-orange-500 text-xl"></i>
+					</div>
+				</div>
+				<span class="text-green-500 font-medium">52 </span>
+				<span class="text-500">novos</span>
+			</div>
+		</div>
+		<div class="col-12 lg:col-6 xl:col-3">
+			<div class="card mb-0">
+				<div class="flex justify-content-between mb-3">
+					<div>
+						<span class="block text-500 font-medium mb-3">Reabertos</span>
+						<div class="text-900 font-medium text-xl">28</div>
+					</div>
+					<div class="flex align-items-center justify-content-center bg-cyan-100 border-round" style="width: 2.5rem; height: 2.5rem">
+						<i class="pi pi-comment text-cyan-500 text-xl"></i>
+					</div>
+				</div>
+				<span class="text-green-500 font-medium">0 </span>
+				<span class="text-500">novos</span>
+			</div>
+		</div>
+		<div class="col-12 lg:col-6 xl:col-3">
+			<div class="card mb-0">
+				<div class="flex justify-content-between mb-3">
+					<div>
+						<span class="block text-500 font-medium mb-3">Concluídos</span>
+						<div class="text-900 font-medium text-xl">152</div>
+					</div>
+					<div class="flex align-items-center justify-content-center bg-purple-100 border-round" style="width: 2.5rem; height: 2.5rem">
+						<i class="pi pi-thumbs-up text-purple-500 text-xl"></i>
+					</div>
+				</div>
+				<span class="text-green-500 font-medium">85 </span>
+				<span class="text-500">novos</span>
+			</div>
+		</div>
 
-        <div class="col-12 xl:col-6">
+		<!-- <div class="col-12 xl:col-6">
             <div class="card">
                 <h5>Atendimentos em Aberto</h5>
                 <DataTable :value="products" :rows="5" :paginator="true" responsiveLayout="scroll">
@@ -195,6 +195,6 @@ watch(
                 <h5>Média de Atendimentos</h5>
                 <Chart type="line" :data="lineData" :options="lineOptions" />
             </div>
-        </div>
-    </div>
+        </div> -->
+	</div>
 </template>
