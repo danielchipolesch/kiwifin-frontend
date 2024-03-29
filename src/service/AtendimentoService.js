dd47e5a92fdea05c5702ee48b6a8e2543b502293
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-03-29 01:06:34 -0300

    Incluída a função para buscar os atendimentos filtrados pelo cliente autenticado.

--- src/service/AtendimentoService.js
+++ src/service/AtendimentoService.js
@@ -1,19 +1,42 @@
 export default class AtendimentoService {
-	getAtendimentosSmall() {
-		return fetch('demo/data/atendimentos-small.json')
+	async buscarAtendimentosPaginaTamanho(page, size) {
+		return fetch(`http://localhost:8080/api/atendimento/listar/filtrado/${page}/${size}`, {
+			method: 'GET'
+		})
 			.then((res) => res.json())
-			.then((d) => d.data);
+			.catch((err) => console.log(err));
 	}
 
-	getAtendimentos() {
-		return fetch('demo/data/atendimentos.json')
+	async buscarAtendimentos() {
+		return fetch(`http://localhost:8080/api/atendimento/listar/filtrado/1/1`, {
+			method: 'GET'
+		})
 			.then((res) => res.json())
-			.then((d) => d.data);
+			.catch((err) => console.log(err));
 	}
 
-	getProductsWithOrdersSmall() {
-		return fetch('demo/data/atendimentos-orders-small.json')
+	async buscarAtendimentosPorCliente(idCliente) {
+		return fetch(`http://localhost:8080/api/atendimento/buscar/cliente/${idCliente}`, {
+			method: 'GET'
+		})
 			.then((res) => res.json())
-			.then((d) => d.data);
+			.catch((err) => console.log(err));
+	}
+
+	async criarAtendimento(body) {
+		try {
+			const res = await fetch('http://localhost:8080/api/atendimento/adicionar', {
+				method: 'POST',
+				body: JSON.stringify(body),
+				headers: { 'Content-type': 'application/json' }
+			});
+			if (res.status === 400) {
+				throw new Error('Erro inesperado');
+			} else {
+				res.json();
+			}
+		} catch (err) {
+			return console.log(err);
+		}
 	}
 }
